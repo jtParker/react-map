609b6f7fd8d88841badd3cfbbd1662e18e640251
JT Parker <jparkerdev@gmail.com>
2018-10-31 21:36:54 -0600

    building list component

---
+++ src/listView.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class listView extends Component {
+  let searchedPlaces = this.props
+
+  render() {
+    <div className="listView-cont">
+
+      searchedPlaces.map((place, index) => {
+        <ul>
+          <li>
+            <h3></h3>
+              <ul>
+                <li></li>
+              </ul>
+          </li>
+        </ul>
+
+      })
+
+    </div>
+  }
+
+}
+
+export default listView
